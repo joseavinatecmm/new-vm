2508115cb6ab79ca4d7f3f3f1019687f25109445
joseavinatecmm <jose.avina@zapopan.tecmm.edu.mx>
2026-04-11 16:23:23 -0600

    tree updated

---
+++ lexycal-analyzer/advance/code-sp-qu.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+   #include "miarchivo.h"
+  #include "mi-a33-r/chivo.h"
+
+int main() { return 0; }
+
---
+++ lexycal-analyzer/advance/source.c
@@ -0,0 +1,8 @@
+      #include <stdio.h>
+int main() {
+   int x = 10;
+   float y = 2.5;
+   if (x > 0) return x;
+   printf("Hola mundo");
+}
+
